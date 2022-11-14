f607c732081fded4c59502c2e09c715588ce7481
NgTuanLoc <tuanloc2352000@gmail.com>
2022-11-14 10:52:21 +0700

    change format

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,5 @@
 public class Main {
    public static void main(String[] args) {
-	  printConversion(75.114);
-   }
-
-   // write your code here
-   public static long toMilesPerHour(double kilometersPerHour) {
-	  if (kilometersPerHour >= 0) {
-		 return Math.round((kilometersPerHour / 1.609));
-	  }
-
-	  return -1;
-   }
-
-   public static void printConversion(double kilometersPerHour) {
-	  long milesPerHour = toMilesPerHour(kilometersPerHour);
-
-	  if (milesPerHour == -1) {
-		 System.out.println("Invalid Value");
-	  } else {
-		 System.out.println(kilometersPerHour + " km/h = " + milesPerHour + " mi/h");
-	  }
+	  System.out.println("Hello");
    }
 }
